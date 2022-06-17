e013e849987a8baa405cfe0fc4d4210e0659c047
Sebastian Klähn <scoreplayer2000@gmail.com>
2022-06-17 20:17:36 +0200

    use local images

--- src/components/wallpaper.vue
+++ src/components/wallpaper.vue
@@ -1,16 +1,18 @@
 <template lang="pug">
 div.cursor-pointer(@click="select_wallpaper")
-    img(:src="post.url")
+    img(:src="converted")
 </template>
 
-<script lang="ts" setup>import { invoke } from '@tauri-apps/api/tauri'
-
+<script lang="ts" setup async>
+import { convertFileSrc, invoke } from '@tauri-apps/api/tauri'
 const props = defineProps({
   post: {
     type: Object,
     required: true,
   },
 })
+
+const converted = await convertFileSrc(props.post.path)
 
 function select_wallpaper() {
   invoke('select_wallpaper', { name: props.post.name })
