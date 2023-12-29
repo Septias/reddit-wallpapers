af73347bdcf33f1e3132976deb2987ddb87fe7fe
Septias <scoreplayer2000@gmail.comclear>
2023-12-29 16:12:11 +0100

    adjust build

--- flake.nix
+++ flake.nix
@@ -1,44 +1,47 @@
-{  
-  description = "Mail Client with TUI written in Rust";  
-  
-  inputs = {  
-    rust-overlay.url = "github:oxalica/rust-overlay";  
-    flake-utils.follows = "rust-overlay/flake-utils";  
-    nixpkgs.follows = "rust-overlay/nixpkgs";  
-    naersk.url = "github:nix-community/naersk";  
-  };  
-  
-  outputs = inputs: with inputs; flake-utils.lib.eachDefaultSystem (system:  
-    let  
-      pkgs = import nixpkgs { overlays = [ (import rust-overlay) ]; inherit system; };  
-      naerskLib = pkgs.callPackage naersk {  
-        cargo = rust-toolchain;  
-        rustc = rust-toolchain;  
-      };  
-      buildInputs = with pkgs; [  
-        openssl  
-      ];  
-      nativeBuildInputs = with pkgs; [ pkg-config dbus freetype libsoup gtk3 webkitgtk cmake ];  
-      rust-toolchain = pkgs.rust-bin.stable.latest.default.override {  
-        extensions = [ "rust-src" "rustfmt" "rust-docs" "clippy" ];  
-      };  
-      LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath buildInputs}";  
-      CARGO_NET_GIT_FETCH_WITH_CLI = "true";  
+{
+  description = "Application to set wallpapers from reddit as desktop-background";
+  inputs = {
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.follows = "rust-overlay/flake-utils";
+    nixpkgs.follows = "rust-overlay/nixpkgs";
+    naersk.url = "github:nix-community/naersk";
+  };
+  outputs = inputs: with inputs; flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { overlays = [ (import rust-overlay) ]; inherit system; };
+      naerskLib = pkgs.callPackage naersk {
+        cargo = rust-toolchain;
+        rustc = rust-toolchain;
+      };
+      buildInputs = with pkgs; [ openssl ];
+      nativeBuildInputs = with pkgs; [ pkg-config dbus openssl freetype libsoup gtk3 webkitgtk ];
+      rust-toolchain = pkgs.rust-bin.stable.latest.default.override {
+        extensions = [ "rust-src" "rustfmt" "rust-docs" "clippy" "rust-analyzer"];
+      };
+      name = "reddit-wallpapers";
     in rec {
       packages = {
-        femail = naerskLib.buildPackage {  
-          name = "femail";  
-          src = ./.;  
-          inherit buildInputs LD_LIBRARY_PATH CARGO_NET_GIT_FETCH_WITH_CLI;  
-          nativeBuildInputs = nativeBuildInputs;  
-        };  
-        default = packages.femail;  
-      };  
-      devShells.default = pkgs.mkShell {  
-        inherit buildInputs LD_LIBRARY_PATH CARGO_NET_GIT_FETCH_WITH_CLI;  
-        nativeBuildInputs = nativeBuildInputs ++ [ rust-toolchain pkgs.rust-analyzer ];  
-        RUST_BACKTRACE = 1;  
-      };  
-    }  
-  );  
-} 
+        ${name} = naerskLib.buildPackage {
+          inherit name buildInputs nativeBuildInputs;
+          src = ./src-tauri;
+          
+          postPatch = ''
+            echo "hi"
+            ls
+          '';
+ 
+          meta = {
+            description = "Application to set wallpapers from reddit as desktop-background";
+            homepage = "https://github.com/Septias/reddit-wallpapers";
+          };
+        };
+        default = packages.${name};
+      };
+      devShells.default = pkgs.mkShell {
+        inherit buildInputs;
+        nativeBuildInputs = nativeBuildInputs ++ [ rust-toolchain ];
+        RUST_BACKTRACE = 1;
+      };
+    }
+  );
+}
